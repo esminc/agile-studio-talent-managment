8cd8c854c8b67a94741c426734b1707be2904b3f
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2025-02-06 22:41:05 +0900

    Add Prettier integration and update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -11,6 +12,7 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat["jsx-runtime"],
+  eslintPluginPrettierRecommended,
   {
     settings: {
       react: {
