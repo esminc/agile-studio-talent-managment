d7c2aae469c593cb8a5c378c59a2065509c06a69
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2025-02-14 00:21:19 +0900

    feat: configure SSR to exclude Radix UI from external dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,12 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
+import pkg from "./package.json";
 
 export default defineConfig({
+  ssr: {
+    noExternal: /^\@radix-ui/,
+  },
   plugins: [
     tailwindcss(),
     !process.env.VITEST && reactRouter(),
