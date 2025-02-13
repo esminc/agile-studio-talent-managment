315f6a78a5a639ffd945500030c131853adf3899
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2025-02-13 09:06:50 +0900

    feat: add Express server setup with React Router integration

---
+++ server.js
@@ -0,0 +1,18 @@
+import { createRequestHandler } from "@react-router/express";
+import express from "express";
+import compression from "compression";
+import morgan from "morgan";
+
+import * as build from "./build/server/index.js";
+
+const app = express();
+app.disable("x-powered-by");
+app.use(compression());
+app.use(express.static("build/client"));
+app.use(morgan("tiny"));
+
+app.all("*", createRequestHandler({ build: build }));
+
+app.listen(3000, () => {
+  console.log("App listening on http://localhost:3000");
+});
